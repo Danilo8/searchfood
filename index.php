dcec95824fa72ebd293eb443de2ca3e5da263ff0
Danilo Gonçalves dos Santos <danilodesenvolvedorweb@hotmail.com>
2018-07-20 10:56:18 -0300

    nova alteração

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="Search Food">
+        <meta name="author" content="Danilo Gonçalves dos Santos">
+        <title>Search Food</title>
+
+        <!--cdn Bootstrap CSS File-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+        <!--CSS.min File-->
+        <link rel="stylesheet" href="css/style-cover.min.css">
+    </head>
+    <body>
+        
+    </body>
 </html>
